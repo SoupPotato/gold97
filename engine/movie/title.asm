fe72218b1203847c61f5814caf877ac73bb6a1b2
pfero <ohpee@loves.dicksinhisan.us>
2020-08-03 23:04:11 +0200

    Move all title screen animation code to engine/movie/title.asm

--- engine/movie/title.asm
+++ engine/movie/title.asm
@@ -4,6 +4,230 @@ TITLE_BORDER_TILE EQU $1C
 TITLE_HOOH_TILE EQU $24
 
 TitleScreen:
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wLYOverrides)
+	ldh [rSVBK], a
+
+	call TitleScreenLoadGFX
+
+	call ChannelsOff
+	call SFXChannelsOff
+
+;; Play starting sound effect
+	;ld de, SFX_TITLE_SCREEN_ENTRANCE
+	;jp PlaySFX
+
+; Play the title screen music.
+	ld de, MUSIC_TITLE
+	call PlayMusic
+.loop
+	ld a, [wJumptableIndex]
+	bit 7, a
+	jr nz, .done
+	call TitleScreenScene
+	call DelayFrame
+	jr .loop
+
+.done
+	pop af
+	ldh [rSVBK], a
+	ret
+
+TitleScreenScene:
+	ld e, a
+	ld d, 0
+	ld hl, .scenes
+	add hl, de
+	add hl, de
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	jp hl
+
+.scenes
+	;dw TitleScreenEntrance
+	dw TitleScreenTimer
+	dw TitleScreenMain
+	dw TitleScreenEnd
+
+TitleScreenEntrance:
+; Animate the logo:
+; Move each line by 4 pixels until our count hits 0.
+	ldh a, [hSCX]
+	and a
+	jr z, .done
+	sub 4
+	ldh [hSCX], a
+
+; Lay out a base (all lines scrolling together).
+	ld e, a
+	ld hl, wLYOverrides
+	ld bc, 8 * 10 ; logo height
+	call ByteFill
+
+; Reversed signage for every other line's position.
+; This is responsible for the interlaced effect.
+	ld a, e
+	xor $ff
+	inc a
+
+	ld b, 8 * 10 / 2 ; logo height / 2
+	ld hl, wLYOverrides + 1
+.loop
+	ld [hli], a
+	inc hl
+	dec b
+	jr nz, .loop
+	ret
+
+.done
+; Next scene
+	ld hl, wJumptableIndex
+	inc [hl]
+	xor a
+	ldh [hLCDCPointer], a
+
+; Play the title screen music.
+	ld de, MUSIC_TITLE
+	call PlayMusic
+
+	ld a, $88
+	ldh [hWY], a
+	ret
+
+TitleScreenTimer:
+; Next scene
+	ld hl, wJumptableIndex
+	inc [hl]
+
+; Start a timer
+	ld hl, wTitleScreenTimer
+	ld de, 73 * 60 + 36
+	ld [hl], e
+	inc hl
+	ld [hl], d
+	ret
+
+TitleScreenMain:
+; Run the timer down.
+	ld hl, wTitleScreenTimer
+	ld e, [hl]
+	inc hl
+	ld d, [hl]
+	ld a, e
+	or d
+	jr z, .end
+
+	dec de
+	ld [hl], d
+	dec hl
+	ld [hl], e
+
+; Save data can be deleted by pressing Up + B + Select.
+	call GetJoypad
+	ld hl, hJoyDown
+	ld a, [hl]
+	and D_UP + B_BUTTON + SELECT
+	cp  D_UP + B_BUTTON + SELECT
+	jr z, .delete_save_data
+
+; To bring up the clock reset dialog:
+
+; Hold Down + B + Select to initiate the sequence.
+	ldh a, [hClockResetTrigger]
+	cp $34
+	jr z, .clock_reset
+
+	ld a, [hl]
+	and D_DOWN + B_BUTTON + SELECT
+	cp  D_DOWN + B_BUTTON + SELECT
+	jr nz, .check_start
+
+	ld a, $34
+	ldh [hClockResetTrigger], a
+	jr .check_start
+
+; Keep Select pressed, and hold Left + Up.
+; Then let go of Select.
+.check_clock_reset
+	bit SELECT_F, [hl]
+	jr nz, .check_start
+
+	xor a
+	ldh [hClockResetTrigger], a
+
+	ld a, [hl]
+	and D_LEFT + D_UP
+	cp  D_LEFT + D_UP
+	jr z, .clock_reset
+
+; Press Start or A to start the game.
+.check_start
+	ld a, [hl]
+	and START | A_BUTTON
+	jr nz, .incave
+	ret
+
+.incave
+	ld a, 0
+	jr .done
+
+.delete_save_data
+	ld a, 1
+
+.done
+	ld [wIntroSceneFrameCounter], a
+
+; Return to the intro sequence.
+	ld hl, wJumptableIndex
+	set 7, [hl]
+	ret
+
+.end
+; Next scene
+	ld hl, wJumptableIndex
+	inc [hl]
+
+; Fade out the title screen music
+	xor a
+	ld [wMusicFadeID], a
+	ld [wMusicFadeID + 1], a
+	ld hl, wMusicFade
+	ld [hl], 8 ; 1 second
+
+	ld hl, wTitleScreenTimer
+	inc [hl]
+	ret
+
+.clock_reset
+	ld a, 4
+	ld [wIntroSceneFrameCounter], a
+
+; Return to the intro sequence.
+	ld hl, wJumptableIndex
+	set 7, [hl]
+	ret
+
+TitleScreenEnd:
+; Wait until the music is done fading.
+
+	ld hl, wTitleScreenTimer
+	inc [hl]
+
+	ld a, [wMusicFade]
+	and a
+	ret nz
+
+	ld a, 2
+	ld [wIntroSceneFrameCounter], a
+
+; Back to the intro.
+	ld hl, wJumptableIndex
+	set 7, [hl]
+	ret
+
+TitleScreenLoadGFX:
 	call DisableLCD
 	call ClearPalettes
 	call ClearSprites
