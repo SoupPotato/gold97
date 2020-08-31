e814ecc85fe8c88d910fe03bcfa3c3df945f956f
toomanyluigis <toomanyluigis11@gmail.com>
2020-08-31 10:53:52 +0100

    Fixed item positions in Magma Shaft

--- maps/MagmaShaft1F.asm
+++ maps/MagmaShaft1F.asm
@@ -45,8 +45,8 @@ MagmaShaft1F_MapEvents:
 	db 0 ; coord events
 
 	db 1 ; bg events
-	bg_event  5, 16, BGEVENT_ITEM, MagmaShaft1FHiddenIceHeal
+	bg_event  5, 12, BGEVENT_ITEM, MagmaShaft1FHiddenIceHeal
 
 	db 2 ; object events
-	object_event  0, 12, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MagmaShaft1FTMRest, EVENT_ICE_PATH_B2F_BLACKTHORN_SIDE_TM_REST
+	object_event  2,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MagmaShaft1FTMRest, EVENT_ICE_PATH_B2F_BLACKTHORN_SIDE_TM_REST
 	object_event 16,  2, SPRITE_GROWLITHE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EnteiEvent, EVENT_RIVAL_AZALEA_TOWN
