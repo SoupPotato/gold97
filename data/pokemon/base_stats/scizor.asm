a3397a1dcc39a7383097d12df23a2320feba2981
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-09-02 16:01:51 -0400

    flying scizor

--- data/pokemon/base_stats/scizor.asm
+++ data/pokemon/base_stats/scizor.asm
@@ -3,7 +3,7 @@
 	db  70, 130, 100,  65,  55,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db BUG, STEEL ; type
+	db BUG, FLYING ; type
 	db 25 ; catch rate
 	db 200 ; base exp
 	db NO_ITEM, NO_ITEM ; items
