ecc3ee858c0faac9105d5f0c71dd813a5d495dec
toomanyluigis <toomanyluigis11@gmail.com>
2020-11-04 23:36:37 +0000

    more Tidal Grotto stuff

--- maps/TidalGrotto1F.asm
+++ maps/TidalGrotto1F.asm
@@ -1,20 +1,40 @@
 	const_def 2 ; object constants
+	const WHIRLISLAND_SUICUNE
 
 TidalGrotto1F_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
+	
+SuicuneEvent:
+	faceplayer
+	opentext
+	writetext SuicuneText
+	cry SUICUNE
+	pause 15
+	closetext
+	setevent EVENT_RIVAL_TEAM_ROCKET_BASE
+	writecode VAR_BATTLETYPE, BATTLETYPE_ROAMING
+	loadwildmon SUICUNE, 40
+	startbattle
+	disappear WHIRLISLAND_SUICUNE
+	reloadmapafterbattle
+	end
 
+SuicuneText:
+	text "Groroa!"
+	done
 
 TidalGrotto1F_MapEvents:
 	db 0, 0 ; filler
 
 	db 2 ; warp events
 	warp_event  3,  2, TIDAL_GROTTO_B1F, 1
-	warp_event 19, 20, AMPARE_CAVERN_B1F, 2
+	warp_event 18, 33, TIDAL_GROTTO_B1F, 2
 
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event 19, 24, SPRITE_GROWLITHE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SuicuneEvent, EVENT_RIVAL_TEAM_ROCKET_BASE
