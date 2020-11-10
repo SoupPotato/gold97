a2d8434cc5f3b8573d29590f0b5249a8d0e0633c
toomanyluigis <toomanyluigis11@gmail.com>
2020-11-10 07:30:19 +0000

    removed fleemons

--- data/wild/flee_mons.asm
+++ data/wild/flee_mons.asm
@@ -2,30 +2,10 @@ FleeMons:
 ; referenced by TryEnemyFlee and FastBallMultiplier
 
 SometimesFleeMons:
-	db MAGNEMITE
-	db GRIMER
-	db TANGELA
-	db MR__MIME
-	db EEVEE
-	db PORYGON
-	db DRATINI
-	db DRAGONAIR
-	db PETICORN
-	db UMBREON
-	db UNOWN
-	db SNUBBULL
 	db -1
 
 OftenFleeMons:
-	db CUBONE
-;	db ARTICUNO
-;	db ZAPDOS
-;	db MOLTRES
-	db DELIBIRD
-	db PHANPY
 	db -1
 
 AlwaysFleeMons:
-;	db RAIKOU
-;	db ENTEI
 	db -1
