214253df2caa48b387ceac65258e1265b62e3cc5
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-12-28 16:48:56 -0500

    :^)

--- engine/events/specials.asm
+++ engine/events/specials.asm
@@ -100,14 +100,14 @@ NameMom:
 	ld b, NAME_MOM
 	ld de, wMomsName
 	farcall _NamingScreen
-	; default to "MOMMY"
+	; default to "MOM"
 	ld hl, wMomsName
 	ld de, .default
 	call InitName
 	ret
 	
 	.default
-	db "MOMMY@"
+	db "MOM@"
 
 NameRater:
 	farcall _NameRater
