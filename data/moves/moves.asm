5a4f0e7834edaa853b6be180154f751d0ebd6746
SoupPotato <toomanyluigis11@gmail.com>
2021-08-13 01:38:11 +0100

    Fixed Psywave

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -160,7 +160,7 @@ Moves:
 	move DIZZY_PUNCH,  EFFECT_CONFUSE_HIT,        70, NORMAL,   100, 10,  20
 	move SPORE,        EFFECT_SLEEP,               0, GRASS,    100, 15,   0
 	move FLASH,        EFFECT_ACCURACY_DOWN,       0, NORMAL,    70, 20,   0
-	move PSYWAVE,      EFFECT_PSYWAVE,             1, PSYCHIC,   80, 15,   0
+	move PSYWAVE,      EFFECT_PSYWAVE,             1, PSYCHIC,  100, 15,   0
 	move SPLASH,       EFFECT_SPLASH,              0, NORMAL,   100, 40,   0
 	move ACID_ARMOR,   EFFECT_DEFENSE_UP_2,        0, POISON,   100, 40,   0
 	move CRABHAMMER,   EFFECT_NORMAL_HIT,         90, WATER,     85, 10,   0
