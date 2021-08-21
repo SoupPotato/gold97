23504ef21fd3728f3400a3f3db276a04170eef2d
SoupPotato <toomanyluigis11@gmail.com>
2021-08-21 18:18:35 +0100

    Fixed Teknos trainer LOS

--- maps/TeknosGym.asm
+++ maps/TeknosGym.asm
@@ -412,13 +412,12 @@ TeknosGym_MapEvents:
 
 	db 0 ; coord events
 
-	db 2 ; bg events
-	bg_event 10, 14, BGEVENT_READ, TeknosGymStatue
+	db 1 ; bg events
 	bg_event  4, 15, BGEVENT_READ, TeknosGymStatue
 
 	db 5 ; object events
 	object_event 16, 13, SPRITE_WHITNEY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, TeknosGymWhitneyScript, 0
-	object_event  4,  8, SPRITE_LASS, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerLassCarrie, 0
+	object_event  4,  8, SPRITE_LASS, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 2, TrainerLassCarrie, 0
 	object_event 11,  3, SPRITE_LASS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 2, TrainerLassBridget, 0
 	object_event 17,  8, SPRITE_BUENA, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBeautySamantha, 0
 	object_event  5, 15, SPRITE_GYM_GUY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, TeknosGymGuyScript, -1
